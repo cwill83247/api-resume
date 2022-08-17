17aee3cdda02fb379f5e282f64ba58d0a2bd6782
cwill83247 <breakthrumusic@hotmail.com>
2022-08-17 10:24:50 +0000

    adding content for the github repo to call

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,5 +1,5 @@
 function userInformationHTML(user) {     //gets called by our promise takes the parameter of user which is the object that is returned from the github api i.e url, name, username etc...
-    //below returns using template literal and the user.html_url and user.login for example are refeencing the object and property retured by API 
+    //below returns using template literal and the user.html_url and user.login for example are refeencing the object and property retured by API and building the html using divs
     return `                                   
         <h2>${user.name}                    
             <span class="small-name">
@@ -13,7 +13,28 @@ function userInformationHTML(user) {     //gets called by our promise takes the 
                 </a>
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
-        </div>`
+        </div>`;
+}
+
+function repoInformationHTML(repos) {       //the repose part is returned by the github api as a value 
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {   //map method is builtin returns an array  and works like a foreach statement to iterate through repos - each one becomes a repo
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+//creating a paragraph below unordered  list ---  and join 
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {             //function name   this what we call ont he onImput event
@@ -32,11 +53,14 @@ function fetchGitHubInformation(event) {             //function name   this what
 //template literals to add a loader div with the image    
 
 $.when(                                                             //start of the promise 
-    $.getJSON(`https://api.github.com/users/${username}`)   //passign in getJSON function   then the variable value of username that we obtained
-).then(
-    function(response) {                                         //function response is built in   - 
-        var userData = response;                                //we store the repsonse in variable userData
+    $.getJSON(`https://api.github.com/users/${username}`),   //passign in getJSON function   then the variable value of username that we obtained
+    $.getJSON(`https://api.github.com/users/${username}/repos`)   //this endpoint is spepcific to the repos 
+    ).then(
+    function(firstResponse, secondResponse) {                      //function firstresponse and second repsonse initally store results form the getJSON call 
+        var userData = firstResponse[0];
+        var repoData = secondResponse[0];                                //we store the repsonse in variable userData and repoData 
         $("#gh-user-data").html(userInformationHTML(userData));   // using Jquery put into the DIV the results from our Function userInformationHTML passign userData as the argument
+        $("#gh-repo-data").html(repoInformationHTML(repoData));     //jquery to call another function repoInformationHTML  ( passing in parameter repoData)
     },
     function(errorResponse) {                                           //error response if statement - this is built into JS
         if (errorResponse.status === 404) {                                 //this is part of syntax  the 404 are standard numbers
